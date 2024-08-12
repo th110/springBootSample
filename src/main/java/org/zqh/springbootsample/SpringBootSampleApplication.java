31b199f8bcc0514845a02b8f9d7f9ed3fbbd79f8
18915263051 <923553443@qq.com>
2024-08-12 11:00:48 +0800

    init

--- src/main/java/org/zqh/springbootsample/SpringBootSampleApplication.java
+++ src/main/java/org/zqh/springbootsample/SpringBootSampleApplication.java
@@ -2,12 +2,19 @@ package org.zqh.springbootsample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootSampleApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootSampleApplication.class, args);
     }
 
+    @GetMapping("hello")
+    public String hello() {
+        return "hello";
+    }
 }
